0b196107065022c0f40de64465fa883c39f7b632
BenBayotas <elijahben.bayotas@gmail.com>
2024-09-09 17:05:13 +0800

    added new database "Teacher" and "Room""

--- schedule/ga_module/models.py
+++ schedule/ga_module/models.py
@@ -7,3 +7,25 @@ class Subject(models.Model):
     subject_name = models.CharField(max_length=255)
     subject_time = models.CharField(max_length=255, null=True)
     subject_day = models.CharField(max_length=255, null=True)
+
+class Teacher(models.Model):
+    teacher_id = models.IntegerField()
+    teacher_name = models.CharField(max_length=255)
+    subject_code1 = models.CharField(max_length=255)
+    subject_code2 = models.CharField(max_length=255)
+    subject_code3 = models.CharField(max_length=255)
+
+class Room(models.Model):
+    LOCALE = {
+        "CB" : "Main",
+        "CBS": "South",
+        "CBE": "East",
+    }
+    ROOM_TYPE = {
+        "LEC": "Lecture",
+        "LAB": "Laboratory",
+    }
+
+    room_id = models.CharField(max_length=255)
+    room_locale = models.CharField(max_length=64, choices=LOCALE, default='CB')
+    room_type = models.CharField(max_length=64, choices=ROOM_TYPE, default='LEC')
