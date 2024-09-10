1b2b7f4993a1a91ed838c3fc5a369a114bcc90f7
BenBayotas <elijahben.bayotas@gmail.com>
2024-09-10 10:22:36 +0800

    deleted old project and started a new one "schedule_project" with app 'scheduler'

--- schedule_project/schedule_project/wsgi.py
+++ schedule_project/schedule_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for schedule project.
+WSGI config for schedule_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schedule.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schedule_project.settings')
 
 application = get_wsgi_application()
--- schedule_project/scheduler/apps.py
+++ schedule_project/scheduler/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GaModuleConfig(AppConfig):
+class SchedulerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ga_module'
+    name = 'scheduler'
